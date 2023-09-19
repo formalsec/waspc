8cf5e302ca2ea5e05a6afc27dcf81e71cd884326
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-09-19 22:47:38 +0100

    fixes non compilation

--- share/libc/wasp.c
+++ share/libc/wasp.c
@@ -1,11 +1,46 @@
 #include "wasp.h"
 
+__attribute__((import_module("summaries"), import_name("alloc"))) void *
+owi_alloc(void *, unsigned int);
+__attribute__((import_module("summaries"), import_name("dealloc"))) void
+owi_dealloc(void *);
+
+extern inline void *__WASP_alloc(void *ptr, unsigned int size) {
+  return owi_alloc(ptr, size);
+}
+
+extern inline void __WASP_dealloc(void *ptr) { return owi_dealloc(ptr); }
+
+__attribute__((import_module("symbolic"), import_name("i32_symbol"))) int
+i32_symbol();
+__attribute__((import_module("symbolic"), import_name("i64_symbol"))) long long
+i64_symbol();
+__attribute__((import_module("symbolic"), import_name("f32_symbol"))) float
+f32_symbol();
+__attribute__((import_module("symbolic"), import_name("f64_symbol"))) double
+f64_symbol();
+
+int __WASP_symb_int(char *name) {
+  return i32_symbol();
+}
+long long __WASP_symb_long(char *name) { return i64_symbol(); }
+float __WASP_symb_float(char *name) { return f32_symbol(); }
+double __WASP_symb_double(char *name) { return f64_symbol(); }
+
+__attribute__((import_module("symbolic"), import_name("assume"))) void
+owi_assume(int);
+__attribute__((import_module("symbolic"), import_name("assert"))) void
+owi_assert(int);
+
+void __WASP_assume(int expr) { owi_assume(expr); }
+void __WASP_assert(int expr) { owi_assert(expr); }
+
 void assume(int expr) { return __WASP_assume(expr); }
 int __WASP_is_symbolic(void *var, unsigned int sz) { return 1; }
+
 /* int __WASP_print_stack(int a) { return 0; } */
 /* void __WASP_print_pc() {} */
 
-/* special boolean ops */
 int and_(int a, int b) {
   __asm__ __volatile__("local.get 0;"
                        "i32.const 0;"
--- share/libc/wasp.h
+++ share/libc/wasp.h
@@ -1,41 +1,25 @@
 #ifndef _WASP_H
 #define _WASP_H
 
-/* memory operations */
-__attribute__((import_module("summaries"), import_name("alloc"))) void *
-__WASP_alloc(void *, unsigned int);
-__attribute__((import_module("summaries"), import_name("dealloc"))) void
-__WASP_dealloc(void *);
+void *__WASP_alloc(void *, unsigned int);
+void __WASP_dealloc(void *);
 
-/* symbolic values */
-__attribute__((import_module("symbolic"), import_name("i32_symbol"))) int
-__WASP_symb_int(char *);
-__attribute__((import_module("symbolic"), import_name("i64_symbol"))) long long
-__WASP_symb_long(char *);
-__attribute__((import_module("symbolic"), import_name("f32_symbol"))) float
-__WASP_symb_float(char *);
-__attribute__((import_module("symbolic"), import_name("f64_symbol"))) double
-__WASP_symb_double(char *);
+int __WASP_symb_int(char *);
+long long __WASP_symb_long(char *);
+float __WASP_symb_float(char *);
+double __WASP_symb_double(char *);
 
-/* symbolic variable manipulation */
-
-__attribute__((import_module("symbolic"), import_name("assume"))) void
-__WASP_assume(int);
-__attribute__((import_module("symbolic"), import_name("assert"))) void
-__WASP_assert(int);
+void __WASP_assume(int);
+void __WASP_assert(int);
 int __WASP_is_symbolic(void *, unsigned int);
 
 void assume(int);
 
-/* debug operations*/
 /* int __WASP_print_stack(int); */
 /* void __WASP_print_pc(); */
 
-/* special boolean ops */
-__attribute__((import_module("symbolic"), import_name("and_"))) int and_(int,
-                                                                         int);
-__attribute__((import_module("symbolic"), import_name("or_"))) int or_(int,
-                                                                       int);
+int and_(int, int);
+int or_(int, int);
 /* int ite(int, int, int); */
 
 #endif
